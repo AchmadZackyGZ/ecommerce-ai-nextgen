f42f4b7bff0973a76eb61dfb2b59c32272dc5c19
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-02-27 20:34:51 +0700

    create SnapResponse in dtos(data transfer object) for delivery token from midtrans and shipped to postman or server or Remix Frontend

---
+++ backend/src/main/java/com/ecommerce/backend/dtos/SnapResponse.java
@@ -0,0 +1,11 @@
+package com.ecommerce.backend.dtos;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SnapResponse {
+    private String token;
+    private String redirectUrl;
+}
