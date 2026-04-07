c41df053a65ed9c3da6edd4d4dfb580c9285df2c
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-04-07 23:32:29 +0700

    adding 2 api gateway on AuthController /me & /profile for editing their user profile itself

--- backend/src/main/java/com/ecommerce/backend/controllers/AuthController.java
+++ backend/src/main/java/com/ecommerce/backend/controllers/AuthController.java
@@ -5,13 +5,17 @@ import com.ecommerce.backend.dtos.AuthRequest;
 import com.ecommerce.backend.dtos.AuthResponse;
 import com.ecommerce.backend.dtos.UserRequest;
 import com.ecommerce.backend.dtos.UserResponse;
+import com.ecommerce.backend.models.User;
 import com.ecommerce.backend.services.AuthService;
 import com.ecommerce.backend.services.UserService;
+
+import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController // INI YANG DICARI OLEH SPRING BOOT!
 @RequestMapping("/api/auth") // URL UTAMANYA
@@ -47,5 +51,22 @@ public class AuthController {
                 .build();
         
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponse<User>> getCurrentUser(Principal principal) {
+        User user = userService.getCurrentUser(principal.getName());
+        return ResponseEntity.ok(ApiResponse.<User>builder().status(200).message("Data profil berhasil diambil").data(user).build());
+    }
+
+    @PutMapping("/profile")
+    public ResponseEntity<ApiResponse<User>> updateProfile(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String phone,
+            @RequestParam(required = false) MultipartFile avatar,
+            Principal principal) {
+        
+        User updatedUser = userService.updateProfile(principal.getName(), name, phone, avatar);
+        return ResponseEntity.ok(ApiResponse.<User>builder().status(200).message("Profil berhasil diperbarui!").data(updatedUser).build());
     }
 }
