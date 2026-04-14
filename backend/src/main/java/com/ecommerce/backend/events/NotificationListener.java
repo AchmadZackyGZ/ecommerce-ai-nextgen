2e8a3ae915f21220e9df4cf655bc77c86cfe1304
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-04-14 22:43:17 +0700

    creating new event when the user have been completed their order, show notification

---
+++ backend/src/main/java/com/ecommerce/backend/events/NotificationListener.java
@@ -0,0 +1,29 @@
+package com.ecommerce.backend.events;
+
+import com.ecommerce.backend.models.Notification;
+import com.ecommerce.backend.repositories.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationListener {
+
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    //  MESIN INI AKAN OTOMATIS MENYALA JIKA ADA EVENT STATUS PESANAN!
+    @EventListener
+    public void handleOrderStatusEvent(OrderStatusEvent event) {
+        Notification notification = Notification.builder()
+                .user(event.getUser())
+                .title(event.getTitle())
+                .message(event.getMessage())
+                .type("STATUS_PESANAN")
+                .isRead(false)
+                .build();
+        
+        notificationRepository.save(notification);
+        System.out.println("🔔 [BACKGROUND] Notifikasi berhasil disimpan untuk user: " + event.getUser().getEmail());
+    }
+}
