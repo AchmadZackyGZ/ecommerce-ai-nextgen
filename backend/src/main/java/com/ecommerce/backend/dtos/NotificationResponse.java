93f849a83300afae3f3abd1e88c4aa8730602a84
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-04-17 12:10:50 +0700

    adding NotificationResponse on dtos(data transfer object) this is response came from server to the frontend

---
+++ backend/src/main/java/com/ecommerce/backend/dtos/NotificationResponse.java
@@ -0,0 +1,16 @@
+package com.ecommerce.backend.dtos;
+
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class NotificationResponse {
+    private Long id;
+    private String title;
+    private String message;
+    private String type;
+    private boolean isRead;
+    private LocalDateTime createdAt;
+}
