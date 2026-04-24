7d6cd6d09d9dd23ddd640deb6055dca198908c9a
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-04-24 21:39:15 +0700

    refactor orderService on orderItemResponse.builder() it

--- backend/src/main/java/com/ecommerce/backend/services/OrderService.java
+++ backend/src/main/java/com/ecommerce/backend/services/OrderService.java
@@ -294,7 +294,7 @@ public class OrderService {
                 OrderItemResponse.builder()
                         .productId(item.getVariant().getProduct().getId()) 
                         .productName(item.getVariant().getProduct().getName() + " (" + item.getVariant().getVariantName() + ")")
-                        .imageUrl(item.getVariant().getProduct().getImageUrl())
+                        .imageUrls(item.getVariant().getProduct().getImageUrls())
                         .quantity(item.getQuantity())
                         .price(item.getPrice())
                         .subTotal(item.getPrice().multiply(new BigDecimal(item.getQuantity())))
