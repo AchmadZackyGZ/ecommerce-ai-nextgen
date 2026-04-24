24b489d0e8d8c98c7eae0c2b0f26c3778b314499
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-04-24 21:40:51 +0700

    refactor on ShopService the part is on productResponse.builder()

--- backend/src/main/java/com/ecommerce/backend/services/ShopService.java
+++ backend/src/main/java/com/ecommerce/backend/services/ShopService.java
@@ -148,7 +148,7 @@ public class ShopService {
                         .id(p.getId())
                         .name(p.getName())
                         .price(p.getPrice())
-                        .imageUrl(p.getImageUrl())
+                        .imageUrls(p.getImageUrls())
                         .category(p.getCategory())
                         .stock(p.getStock())
                         .build())
