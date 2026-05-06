353eaee826a44dbe2a9b79b08854e96d57adc14b
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-05-06 23:00:46 +0700

    adding new Query and directily convert using jpaRepository on ReviewRepository

--- backend/src/main/java/com/ecommerce/backend/repositories/ReviewRepository.java
+++ backend/src/main/java/com/ecommerce/backend/repositories/ReviewRepository.java
@@ -1,5 +1,6 @@
 package com.ecommerce.backend.repositories;
 
+import com.ecommerce.backend.models.Order;
 import com.ecommerce.backend.models.Product;
 import com.ecommerce.backend.models.Review;
 import com.ecommerce.backend.models.User;
@@ -20,6 +21,9 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     // 🔥 FITUR BARU: Mengecek apakah user sudah pernah mereview produk ini (Anti-Spam)
     boolean existsByProductAndUser(Product product, User user);
+
+    // 🔥 RADAR BARU: Cek apakah Produk ini sudah di-review di Pesanan (Order) yang ini?
+    boolean existsByOrderAndProduct(Order order, Product product);
 
     // 🔥 MENGHITUNG RATA-RATA BINTANG SELURUH PRODUK DI SATU TOKO
     @org.springframework.data.jpa.repository.Query("SELECT COALESCE(AVG(r.rating), 0.0) FROM Review r WHERE r.product.shop.id = :shopId")
